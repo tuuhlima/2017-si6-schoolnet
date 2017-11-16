54cbbfee27d12d7b49f62542e6d00dfbe15f9c20
Túlio lima <tuuhlima@yahoo.com.br>
2017-11-16 21:31:27 -0200

    add server.js finish

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const PORT = 3000
 
-app.get('/', (request, response) => response.send("Hi there"))
+//express setup
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(express.static('public'))
+
+//templates
+app.set('views',path.join(__dirname,'public'))
+app.set('view engine','ejs')
+
+app.get('/', (request, response) => {
+    response.render('index')
+})
 
 app.listen(PORT,() => { 
     console.log('Server is running on port' + PORT)
